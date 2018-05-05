b8f1df69596e027e6ec92740062780e40647c14a
Genevieve Mauger <genevieve_mauger@live.com.au>
2018-05-05 22:40:11 +1000

    Fixed image_cache issue, images still not saving however

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,7 +11,7 @@ class ProfilesController < ApplicationController
   # GET /profiles/1
   # GET /profiles/1.json
   def show
-    # @photo = Photo.new
+    @photos = Photo.find(params[:id])
   end
 
   # GET /profiles/new
@@ -79,6 +79,6 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :mobile, :street_address, :suburb, :postcode, :country, :mobile, :image, :latitude, :longitude, :user_id)
+      params.require(:profile).permit(:first_name, :last_name, :mobile, :street_address, :suburb, :postcode, :country, :mobile, :image_data, :latitude, :longitude, :user_id)
     end
 end
--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,9 +1,10 @@
 require "shrine"
 require "shrine/storage/file_system"
+require "image_processing/mini_magick"
 
 Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"), # permanent
+  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
+  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"),
 }
 
 Shrine.plugin :activerecord
