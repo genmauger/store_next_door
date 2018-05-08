fed833ea8a127b21cf7a21e5d9e2c4e4881a4173
Genevieve Mauger <genevieve_mauger@live.com.au>
2018-05-08 23:12:10 +1000

    Added resources:charges to config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :facility_spaces
   resources :storage_facilities
   resources :profiles
+  resources :charges
   devise_for :users, controllers: { registrations: "registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
