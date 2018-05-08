665fcad0b148ea537d9a0e21a133d9e186a7ca93
Genevieve Mauger <genevieve_mauger@live.com.au>
2018-05-09 00:08:54 +1000

    Stripe payment works, added current email method to payment form

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,26 +1,36 @@
 class ChargesController < ApplicationController
   
   def new
+    @email = current_user.email
   end
   
-  def create
-    # Amount in cents
-    @amount = 500
-  
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
-  
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
-  
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
+  def charge
+    # amount = @sneaker.price.to_i
+
+    if current_user.stripe_id.blank?
+        customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+      )
+      current_user.stripe_id = customer.id
+      current_user.save!
+    end
+
+      Stripe::Charge.create(
+        customer: current_user.stripe_id,
+        amount: amount,
+        # description: @sneaker.description,
+        currency: 'AUD'
+      )
+
+      # current_user.charges << Charge.new(charge_id: charge.id)
+      
+      flash[:notice] = "Payment made!"
+      redirect_back fallback_location: sneakers_path
+
+      rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_back fallback_location: sneakers_path
   end
 end
+
