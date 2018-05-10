3c3b9d34c9c324e1652513a66976c81f5aa300e7
Genevieve Mauger <genevieve_mauger@live.com.au>
2018-05-11 00:08:55 +1000

    Got rid of mailer methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,19 +3,4 @@ class PagesController < ApplicationController
   def index
   end
 
-  def contact
-  end
-
-  def contact_email
-    user = current_user
-    ContactMailer.send_contact_email(user: user, params: email_params).deliver_now
-    render :contact
-  end
-
-  private 
-
-  def email_params
-    params.require(:contact).permit(:name, :message)
-  end
-
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch('SYSTEM_EMAIL')
-  layout 'mailer'
 end
 
